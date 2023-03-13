3c89075676a2e06801ac3d3360240658d204bb6d
jrm2087 <jrm2087@gmail.com>
2023-03-13 11:11:02 -0500

    view

--- apps/user/views.py
+++ apps/user/views.py
@@ -8,13 +8,14 @@ from .models import User
 def listado_users(request):
     lista_usuarios = User.objects.all()
     titulo = 'Listado de usuarios 2'
-    return render(request, 'user/users2.html', {'lista_usuarios': lista_usuarios, 'titulo': titulo})
+    return render(request, 'user/users.html', {'lista_usuarios': lista_usuarios, 'titulo': titulo})
 
 
 class ListUser(ListView):
     context_object_name = 'lista_usuarios'
+    queryset = User.objects.all()
     template_name = 'user/users.html'
-    
+
     def get_context_data(self, *, object_list=None, **kwargs):
         titulo = 'Listado de usuarios 1'
         context = {
@@ -23,5 +24,5 @@ class ListUser(ListView):
         kwargs.update(context)
         return super().get_context_data(object_list=object_list, **kwargs)
 
-    def get_queryset(self):
-        return User.objects.all()
+    # def get_queryset(self):
+    #     return User.objects.all()
